2a1b459d485f7d74360c2b808960c001f26ef8b0
Josef Sevcik <josevcik@vendavo.com>
2024-08-10 21:28:01 +0200

    fix ts

--- src/server.ts
+++ src/server.ts
@@ -29,7 +29,7 @@ const start = async (): Promise<void> => {
   if (process.env.NEXT_BUILD) {
     app.listen(PORT, async () => {
       payload.logger.info(`Next.js is now building...`)
-      // @ts-ignore
+      // @ts-expect-error
       await nextBuild(path.join(__dirname, '..'))
       process.exit()
     })
